45b3babd10bc163031520aea81243b55606b9c2b
bwees <brandonwees@gmail.com>
2025-08-24 18:28:03 -0500

    firecontrol 21.1.5

---
+++ Casks/firecontrol.rb
@@ -0,0 +1,16 @@
+cask "firecontrol" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "21.1.5"
+  sha256 :no_check
+
+  url "https://assets.langmuirsystems.com/firecontrol/latest/FireControl-#{version}.pkg"
+  name "FireControl"
+  desc "Control software for Langmuir plasma cutters"
+  homepage "https://www.langmuirsystems.com/downloads"
+
+  pkg "FireControl-#{version}.pkg"
+
+  uninstall pkgutil: "com.langmuir.cnc.desktop"
+
+end
